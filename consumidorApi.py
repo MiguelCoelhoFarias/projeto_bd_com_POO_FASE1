a9fb16cced6c6f0d8ca7332dfe61b3d947b2189e
Miguel Farias <miguelcoelho500@gmail.com>
2023-09-28 11:29:05 -0300

    repositorio contendo conexao ao mongo e convertendo para arquivo json

--- consumidorApi.py
+++ consumidorApi.py
@@ -1,15 +1,27 @@
+# import basedosdados as bd
+# import pandas as pd
+# import requests
+# import psycopg2
+# import csv
+# # api gratuita que consome dados de usuarios de bandalarga fixa no brasil contendo a média por mês
+# df = bd.read_table(dataset_id='br_anatel_banda_larga_fixa',
+#                    table_id='densidade_brasil',
+#                    billing_project_id="projetobandalargafixa")
+# # acessando as tabelas da api
+# # usando o id do projeto criado na conta gcloud com meu email institucional
+
+# # Transforma o DataFrame em um arquivo CSV
+# df.to_csv('dados.csv', index=False)
+# print("Dados exportados para dados.csv")
+
 import basedosdados as bd
 import pandas as pd
-import requests
-import psycopg2
-import csv
+
 # api gratuita que consome dados de usuarios de bandalarga fixa no brasil contendo a média por mês
 df = bd.read_table(dataset_id='br_anatel_banda_larga_fixa',
                    table_id='densidade_brasil',
                    billing_project_id="projetobandalargafixa")
-# acessando as tabelas da api
-# usando o id do projeto criado na conta gcloud com meu email institucional
 
-# Transforma o DataFrame em um arquivo CSV
-df.to_csv('dados.csv', index=False)
-print("Dados exportados para dados.csv")
+# Transforma o DataFrame em um arquivo JSON
+df.to_json('dados.json', orient='records', lines=True)
+print("Dados exportados para dados.json")
--- main.py
+++ main.py
@@ -1,44 +1,23 @@
+import basedosdados as bd
 import pandas as pd
-import mysql.connector as mysql
-import csv
 
+# api gratuita que consome dados de usuários de banda larga fixa no Brasil contendo a média por mês
+df = bd.read_table(dataset_id='br_anatel_banda_larga_fixa',
+                   table_id='densidade_brasil',
+                   billing_project_id="projetobandalargafixa")
 
-# obtendo conexão com o banco
-def getConnection():
-    conn = mysql.connect(
-        host='127.0.0.1', database='percistenciadedados', user='root', password='root123')
-    return conn
+# Transforma o DataFrame em um arquivo JSON com orientação 'records'
+df.to_json('dados.json', orient='records', lines=True)
 
-# inserindo dados com formatação no mySQL
+# Abre o arquivo JSON gerado e lê cada linha para formatá-lo corretamente
+with open('dados.json', 'r') as json_file:
+    lines = json_file.readlines()
 
+# Remove quebras de linha e vírgula no final da última linha
+formatted_data = '[' + ','.join(lines) + ']'
 
-def insertCsvToMySQL():
-    conn = getConnection()
+# Reescreve o arquivo JSON com a formatação correta
+with open('dados.json', 'w') as json_file:
+    json_file.write(formatted_data)
 
-    with open("dados.csv") as arquivoCSV:
-        dadosCSV = csv.reader(arquivoCSV, delimiter=",")
-        dadosToMySQL = []
-
-        for row in dadosCSV:
-            dadosToMySQL.append((row[0], row[1], row[2]))
-
-        sql_stm = "INSERT INTO usuarios (ano, mes, usuarios) VALUES (%s, %s, %s)"
-        cur = conn.cursor()
-        cur.executemany(sql_stm, dadosToMySQL)
-        conn.commit()
-        cur.close()
-        conn.close()
-
-# def feito para testar a conexão
-
-
-def testarConnection():
-    conn = mysql.connect(
-        host='127.0.0.1', database='percistenciadedados', user='root', password='root123')
-    print(conn.get_server_info())
-    print(conn.get_server_version())
-
-
-# rodando aplicacao
-if __name__ == '__main__':
-    insertCsvToMySQL()
+print("Dados exportados para dados.json")
